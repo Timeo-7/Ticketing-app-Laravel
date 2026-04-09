f8afeeda6edff3a6f1223d5ca847be87e6f0b265
--replace-all <franville@et.esiea.fr>
2026-04-09 15:25:06 +0200

    README

--- Ticketing-app-Laravel/routes/api.php
+++ Ticketing-app-Laravel/routes/api.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['web', 'auth'])->group(function () {
     Route::post('/tickets', [TicketController::class, 'storeApi'])->name('api.tickets.storeApi');
     Route::put('/tickets/{id}', [TicketController::class, 'updateApi']);
-    Route::put('/tickets/{id}/validate', [TicketController::class, 'validateApi']);
-    Route::delete('/tickets/{id}', [TicketController::class, 'deleteApi']);
 });
